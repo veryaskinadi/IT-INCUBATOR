a6632b2d08041de58998f52789db5e17f2ec9651
Dilyara <khakimovadi@gmail.com>
2023-05-14 12:17:16 +0300

    add flowers

---
+++ Flowers/index.js
@@ -0,0 +1,8 @@
+function lovefunc(flower1, flower2){
+    if (((flower1 % 2 == 0) && (flower2 % 2 == 1)) || ((flower2 % 2 == 0) && (flower1 % 2 == 1))) {
+      return true
+    } else {
+      return false
+    }
+}
+console.log(lovefunc())
