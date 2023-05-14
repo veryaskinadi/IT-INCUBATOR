00d29b96febb35e1ff211f849e46b41749d6a2ff
Dilyara <khakimovadi@gmail.com>
2023-05-14 15:07:39 +0300

    add select query

--- query.sql
+++ query.sql
@@ -1,5 +1,5 @@
 select 
-    sqrt(number1) as root 
+    sqrt(number1) as root,
     log(number2) 
 from 
     decimals
@@ -11,3 +11,42 @@ select
 from 
     decimals
 ;
+
+select 
+  id,
+  name, 
+  birthday,
+  Lower(race) race
+  
+from 
+  demographics
+;
+
+
+select * 
+from 
+  companies
+order by
+  employees DESC
+;
+
+select 
+  to_hex(arms) as arms,
+  to_hex(legs) as legs
+from 
+  monsters
+;
+
+
+select 
+  name,
+  age,
+  semester,
+  mentor, 
+  tuition_received
+from students
+  where tuition_received = false;
+
+
+INSERT into participants (name, age, attending) values ('Dilyara', 33, true);
+SELECT * FROM participants;
